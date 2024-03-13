9db5a6533180e2723e5a27c1c5c39595d16c20ce
realJimboSlice <jimm7493@stud.kea.dk>
2024-03-13 15:33:33 +0100

    Added comments (very important)

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var uniqueInOrder = function (iterable) {
+  // Declare empty array
   inOrderArr = [];
+  // Loop through the iterable
   for (let i = 0; i < iterable.length; i++) {
+    // If the current element is not equal to the next element, push it to the inOrderArr
     if (iterable[i] !== iterable[i + 1]) {
       inOrderArr.push(iterable[i]);
     }
